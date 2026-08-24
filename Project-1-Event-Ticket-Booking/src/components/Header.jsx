f8aa4f123b9a70f259a9db07df57988f5bdde15d
Ashvanth5iytij <vtu25807@veltech.edu.in>
2026-08-24 21:35:56 +0530

    Add files via upload

---
+++ Project-1-Event-Ticket-Booking/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(){
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="event-title">TechNova 2026 - Ticket Booking</h1>
+      </div>
+    </header>
+  )
+}
